cd7186df8658d784979af4150735121ab5f7e47c
Denys Petryniak <denys.petryniak@gmail.com>
2023-11-27 12:59:28 +0200

    Accept messages in the AI endpoint

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
   const { OPENAI_API_KEY } = useRuntimeConfig();
 
   const openai = new OpenAI({
@@ -9,8 +10,9 @@ export default defineEventHandler((event) => {
 
   async function main() {
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
       model: "gpt-3.5-turbo",
+      messages: body.messages || [],
+      temperature: body.temperature || 1,
     });
 
     // TODO: tidy up
