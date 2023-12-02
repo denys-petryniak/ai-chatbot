8bf342def775973c0b09baa388ad39c3dbd8e651
Denys Petryniak <denys.petryniak@gmail.com>
2023-12-02 16:57:15 +0200

    Refactor fetchWithTimeout util

--- utils/index.ts
+++ utils/index.ts
@@ -1,17 +1,24 @@
 import type { NitroFetchOptions } from "nitropack";
+
+const TIMEOUT_DURATION = 50_000; // 50,000 milliseconds = 50 seconds
+
 export async function fetchWithTimeout<T>(
   url: string,
   fetchOptions: NitroFetchOptions<any, any> = {}
 ): Promise<T> {
   const controller = new AbortController();
-  const id = setTimeout(() => {
+
+  const timeoutId = setTimeout(() => {
     controller.abort();
-    throw new Error("Requet timed out");
-  }, 50_000);
+    throw new Error("Request timed out");
+  }, TIMEOUT_DURATION);
+
   const res = await $fetch<T>(url, {
     ...fetchOptions,
     signal: controller.signal,
   });
-  clearTimeout(id);
+
+  clearTimeout(timeoutId);
+
   return res;
 }
